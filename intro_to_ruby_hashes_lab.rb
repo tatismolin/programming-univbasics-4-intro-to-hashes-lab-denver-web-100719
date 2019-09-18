b4f7d024ef2d3ddaf9db6621bcbba03fa2711594
Tatiana Smolin <tatismolin@gmail.com>
2019-09-18 21:21:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,22 +1,41 @@
 def new_hash
   # return an empty hash
+  new_hash = {}
 end
+new_hash
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    name: "tanya",
+    age: 30
+  }
 end
+my_hash[:age]
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name: "Grace Hopper"
+  }
 end
+pioneer[:name]
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    id: 3
+    }
 end
+id_generator[:id]
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  hash = {
+    key: value
+  }
 end
+
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
